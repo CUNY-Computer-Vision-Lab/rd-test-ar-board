ee5601685255e38225a8cceed8b858260522229b
Tyler Franklin <get@tylerfranklin.com>
2019-07-03 15:20:22 -0400

    Add some other obj refs and labels

--- AR Board/ObjectFacts.swift
+++ AR Board/ObjectFacts.swift
@@ -14,13 +14,25 @@ struct ObjectFact {
     let name: String
 
     static let WaterBottle = ObjectFact(facts: ["utrem aquae", "H2O", "mmm, water"],
-                                            titlePosition: (-0.2, 0.2),
-                                            name: "water bottle")
+                                        titlePosition: (-0.2, 0.2),
+                                        name: "water bottle")
+
+    static let SoloCup = ObjectFact(facts: ["it's red", "it holds liquids", "or whatever"],
+                                    titlePosition: (-0.2, 0.2),
+                                    name: "solo cup")
+
+    static let Mouse = ObjectFact(facts: ["clicky input device", "invented by xerox", "origin of name unknown"],
+                                    titlePosition: (-0.2, 0.2),
+                                    name: "mouse")
 
     static func facts(for name:String) -> ObjectFact? {
         switch name {
         case "water bottle":
             return .WaterBottle
+        case "solo cup":
+            return .SoloCup
+        case "mouse":
+            return .Mouse
         default:
             return nil
         }
